86adbaa9fd82736e34fa5d3427de44ca357afff3
Akashpreet Singh <akashpreetsingh739@gmail.com>
2025-03-19 16:20:30 +0000

    VAT and Website added in API POST request

--- myproject/organization/models.py
+++ myproject/organization/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from district.models import District
 from municipality.models import Municipality
 
+
+
+
 class Organization(models.Model):
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     municipality = models.ForeignKey(Municipality, on_delete=models.CASCADE)
@@ -9,16 +12,18 @@ class Organization(models.Model):
     street = models.CharField(max_length=510)
     postal_code = models.CharField(max_length=10)
     city = models.CharField(max_length=255)
-    email = models.EmailField(unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
     created_by = models.CharField(max_length=255)
     created_date = models.DateTimeField()
     last_modified_by = models.CharField(max_length=255, null=True, blank=True)
     last_modified_date = models.DateTimeField(null=True, blank=True)
-    vat_number = models.CharField(max_length=20, null=True, blank=True)
-    website = models.URLField(null=True,blank=True)
+    vat_number = models.CharField(max_length=20,unique=True)
+    website = models.URLField(null=True, blank=True)
 
-    
+
+
+
     class Meta:
         db_table = 'organization'
 
