1f6ac724baba2394cbaae53cee70bfc2a40fe3d4
gabriel-almeida-a22203311 <126700937+gabriel-almeida-a22203311@users.noreply.github.com>
2025-05-30 18:48:46 +0100

    boolean anuncio ativo

--- myproject/announcements/models.py
+++ myproject/announcements/models.py
@@ -9,8 +9,9 @@ class Announcement(models.Model):
     created_date = models.DateTimeField()
     last_modified_by = models.CharField(max_length=255, null=True, blank=True)
     last_modified_date = models.DateTimeField(null=True, blank=True)
-    expiry_date = models.DateTimeField()
+    expiry_date = models.DateTimeField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
     
 
 
