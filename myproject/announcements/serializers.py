00263ec3eaff52b1e21be24a0e2cf65cc66d0594
Akashpreet Singh <akashpreetsingh739@gmail.com>
2025-06-14 19:32:41 +0100

    Get announcement with property details

--- myproject/announcements/serializers.py
+++ myproject/announcements/serializers.py
@@ -1,7 +1,11 @@
+# announcements/serializers.py
 from rest_framework import serializers
 from .models import Announcement
+from property.serializers import PropertySerializer  # Import here
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    property = PropertySerializer(read_only=True)  # Nest it
+
     class Meta:
         model = Announcement
-        fields = "__all__"  
+        fields = "__all__"
