aff586acd0cc8f36bfba3cac3262903efc7271fa
gabriel-almeida-a22203311 <126700937+gabriel-almeida-a22203311@users.noreply.github.com>
2025-06-14 20:40:23 +0100

    typo

--- myproject/property/filters.py
+++ myproject/property/filters.py
@@ -4,8 +4,8 @@ from .models import Property
 class PropertyFilter(django_filters.FilterSet):
     district = django_filters.NumberFilter(field_name="district__id", lookup_expr="exact")
     municipality = django_filters.NumberFilter(field_name="municipality__id", lookup_expr="exact")
-    area_bruta = django_filters.NumberFilter(field_name="area_bruta", lookup_expr='exact')
-    area_util = django_filters.NumberFilter(field_name="area_util", lookup_expr='exact')
+    area_bruta = django_filters.NumberFilter(field_name="area_bruta", lookup_expr='gte')
+    area_util = django_filters.NumberFilter(field_name="area_util", lookup_expr='gte')
     nova_construcao = django_filters.CharFilter(field_name="nova_construcao", lookup_expr='iexact')
     property_type = django_filters.CharFilter(field_name="property_type", lookup_expr='iexact')
     preco_minimo = django_filters.NumberFilter(field_name="preco_minimo", lookup_expr='gte')
