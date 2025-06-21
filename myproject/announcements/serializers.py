d6c986033b420aa54505f6d9ecff313912ac85fd
Akashpreet Singh <akashpreetsingh739@gmail.com>
2025-06-21 13:24:23 +0100

    announcements bug fix

--- myproject/announcements/serializers.py
+++ myproject/announcements/serializers.py
@@ -1,11 +1,21 @@
 # announcements/serializers.py
 from rest_framework import serializers
 from .models import Announcement
-from property.serializers import PropertySerializer  # Import here
+from property.serializers import PropertySerializer
 
 class AnnouncementSerializer(serializers.ModelSerializer):
-    property = PropertySerializer(read_only=True)  # Nest it
+    property = serializers.SerializerMethodField()
 
     class Meta:
         model = Announcement
         fields = "__all__"
+
+    def get_property(self, obj):
+        request = self.context.get('request')
+        method = request.method if request else None
+
+        # Show full property details only on GET requests
+        if method in ['GET']:
+            return PropertySerializer(obj.property, context=self.context).data
+        else:
+            return obj.property.id
